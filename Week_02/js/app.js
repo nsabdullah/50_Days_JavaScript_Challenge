c87005112a550957fc7ee2fd5eff87e23f161e28
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-11-20 11:32:11 +0600

    js

--- Week_02/js/app.js
+++ Week_02/js/app.js
@@ -1,1 +1,18 @@
-// old version we can write java script code in this way
+
+
+let ageCheck = (age) => {
+  if (age < 18) {
+    return "You are child.";
+  } else {
+    return "You are adult.";
+  }
+};
+console.log(ageCheck(36));
+
+
+
+ let test = (a, b) => {
+  return a + b;
+}
+console.log(test(3, 4));
+
